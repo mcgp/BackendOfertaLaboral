272961b31405ebd671970870199d93576aeee38d
mcgp <maria.1701614679@ucaldas.edu.co>
2021-04-10 02:54:41 -0500

    Subir archivos al servidor

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -4,4 +4,11 @@ export namespace Keys {
   export const tiempoVencimientoJWT = Math.floor(Date.now()/1000) +(60*60*8);
   export const claveSecretaJWT = 'jwt@Prog3';
   export const twilioPhone = '+13235082989'
+  export const carpetaImagenPersonas = '../../archivos/personas';
+  export const nombreCampoImagenPersona = 'file';
+  export const extensionesPermitidasIMG: string[] = ['.PNG','.JPG','.JPEG','.SVG'];
+  export const tamMaxImagenPersona = 1024*1024;
+  export const carpetaDocumentoPersona = '../../archivos/documentos';
+  export const nombreCampoDocumentoPersona = 'file';
+  export const extensionesPermitidasDOC: string[] = ['.PDF','.DOC','.DOCX','.XLS'];
 }
